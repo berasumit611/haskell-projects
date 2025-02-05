60d6f84cfd4d2e21f1097cee3069ce6e3a755b00
aiquant4 <sumit.bera@aiquantgroup.com>
2025-02-05 10:28:04 +0530

    problem3  added

--- problem3/app/Main.hs
+++ problem3/app/Main.hs
@@ -1,6 +1,20 @@
+-- problem 3 Find the K'th element of a list.
+-- λ> elementAt [1,2,3] 2
+-- 2
+-- λ> elementAt "haskell" 5
+-- 'e'
+
 module Main (main) where
 
-import Lib
+elementAt::[a]->Int->a
+elementAt [] _=error "Index out of bounds"
+elementAt(x:_) 1= x --the first element in haskell is indexed as 1
+elementAt (_ :xs) k
+    | k < 1 = error "Index out of bounds"
+    | otherwise = elementAt xs (k-1)
+
 
 main :: IO ()
-main = someFunc
+main = do
+    print $ elementAt[1,2,3] 2
+    print $ elementAt "haskell" 5
